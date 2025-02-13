810a3735eada85ee8ff9ac6def410f28c442e0ab
noodlewd <michel35@naver.com>
2025-02-14 00:29:49 +0900

    fix: context value 추가

--- src/context/HealthProvider.jsx
+++ src/context/HealthProvider.jsx
@@ -1,8 +1,11 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const HealthContext = createContext(null);
 const HealthProvider = ({ children }) => {
-  return <HealthContext.Provider>{children}</HealthContext.Provider>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return <HealthContext.Provider value={(email, setEmail, password, setPassword)}>{children}</HealthContext.Provider>;
 };
 
 export default HealthProvider;
