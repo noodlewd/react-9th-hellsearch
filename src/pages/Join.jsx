1b6e4214492974892af190dca8d940a98521f40a
noodlewd <michel35@naver.com>
2025-02-14 00:29:18 +0900

    feat: 회원가입, 뒤로가기 추가 / supabase auth users연동

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import { useContext, useState } from "react";
+import supabase from "../supabase/client";
+import { useNavigate } from "react-router-dom";
+import { HealthContext } from "../context/HealthProvider";
 
 const Join = () => {
-  return <div>Join</div>;
+  const { email, setEmail, password, setPassword } = useContext(HealthContext);
+  const navigate = useNavigate();
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    try {
+      await supabase.auth.signUp({
+        email,
+        password,
+      });
+
+      alert("회원가입이 완료되었습니다. 로그인 페이지로 이동합니다.");
+      navigate("/");
+    } catch (error) {
+      alert(error.message);
+      console.error("회원가입 오류:", error);
+    }
+  };
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div>
+      <h2>회원가입 페이지</h2>
+      <form onSubmit={handleSignup}>
+        <input type="email" placeholder="이메일" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">회원가입</button>
+        <button onClick={handleBack}>뒤로가기</button>
+      </form>
+    </div>
+  );
 };
 
 export default Join;
