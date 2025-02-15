566d78cbb609e1ac6f3a44782ae0352945f01b05
mzsoba <owt4832@gmail.com>
2025-02-15 20:54:14 +0900

    test: test용

--- src/pages/FeedAdd.jsx
+++ src/pages/FeedAdd.jsx
@@ -1,7 +1,17 @@
-import React from "react";
-
 const FeedAdd = () => {
-  return <div>FeedAdd</div>;
+  const onSubmitHandler = () => {
+    
+  }
+  return <>
+  <div>
+    <form onSubmit={onSubmitHandler}>
+      <input type="text" placeholder="제목"></input>
+      <input type="text"></input>
+      <input type="text" placeholder="내용"></input>
+      <button>포스팅</button>
+    </form>
+  </div>
+  </>
 };
 
 export default FeedAdd;
