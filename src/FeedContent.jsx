43f2a0b58ca1e9d27ba711d7176ad7862d3b0396
ick-web <yih000098@gmail.com>
2025-02-15 19:05:18 +0900

    feat: 좋아요 버튼, ... 버튼 수정,삭제 드롭다운 기능 추가

--- src/FeedContent.jsx
+++ src/FeedContent.jsx
@@ -1,45 +1,70 @@
-import React, { useEffect, useState } from 'react'
-import supabase from './supabase/client'
+import React, { useEffect, useState } from "react";
+import supabase from "./supabase/client";
 
-const FeedContent = () => {
-  const [like,setLike] = useState(0);
-  const [istoggled,setIsToggled] = useState(false);
+const FeedContent = ({ feed }) => {
+  const [feeds, setFeeds] = useState([]); // 피드 담을 배열
+  const [likes, setLikes] = useState(0); // 좋아요 상태 관리
+  const [isToggled, setIsToggled] = useState(false); // 버튼 토글 상태 관리
+  const [isOpen, setIsOpen] = useState(false);
 
   // feed 테이블에서 userid를 가진 데이터 가져오기
   useEffect(() => {
     const fetchData = async () => {
-      const { data: feeds, error } = await supabase
-  .from('feeds')
-  .select('*')
-  if(error){
-    console.error('Error fetching data:',error)
-  } 
-    }
-  fetchData();
-  }, [])
+      const { data: feedsData, error } = await supabase
+        .from("feeds")
+        .select("*");
+      if (error) {
+        console.error("데이터 불러오기 실패:", error);
+      } else {
+        setFeeds(feedsData);
+      }
+    };
+    fetchData();
+  }, []);
 
-  const likeBtnHandler = () => {
-    setIsToggled(prevState => !prevState);
+  //드롭다운 버튼
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
   }
-  
+
+  // 좋아요 버튼 핸들러
+  const likeBtnHandler = (id) => {
+    setIsToggled((prevState) => ({
+      ...prevState[id],
+    }));
+
+    setLikes((prevLikes) => ({
+      ...prevLikes,
+      [id]: prevLikes[id] ? prevLikes[id] - 1 : 1, //좋아요 증가/감소
+    }));
+  };
 
   return (
-    <div key={feeds.user_id}>
-        <p>{feeds.nickname}</p>
-        <img src={feeds.content_img} alt="" />
+    <div>
+      <div key={feed.id}>
+        <p>{feed.nickname}</p>
+        {/* 이미지가 있으면 출력하고 없으면 아무것도 출력안함 */}
+        {feed.content_img && <img src={feed.content_img} alt="피드 이미지" />} 
         <div>
-          <p>{feeds.title}</p>
-          {like}
-          <button onClick={likeBtnHandler}>
-            {istoggled ? '꺼짐':'켜짐'}
-            </button>
+          <p>{feed.title}</p>
+          <p>좋아요: {likes[feed.id] || 0}</p>
+          <button onClick={() => likeBtnHandler(feed.id)}>
+            {isToggled[feed.id] ? "좋아요 취소" : "좋아요"}
+          </button>
           <button>댓글 달기</button>
         </div>
-        <button>...(수정,삭제)</button>
+        <button onClick={toggleDropdown}>
+        {isOpen ? '닫기' : '...'}
+      </button>
+      {isOpen && (
+        <div>
+          <button>수정</button>
+          <button>삭제</button>
+        </div>
+      )}
     </div>
-  )
-}
+    </div>
+  );
+};
 
-
-export default FeedContent
-
+export default FeedContent;
