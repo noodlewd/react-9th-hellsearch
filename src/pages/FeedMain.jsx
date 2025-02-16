147416e5184a36bafbf26ef7e8a8172a6a1790fe
mzsoba <owt4832@gmail.com>
2025-02-16 17:04:30 +0900

    feat: BottomScrollListener 를 사용한 무한 스크롤 구현 주석참고

--- src/pages/FeedMain.jsx
+++ src/pages/FeedMain.jsx
@@ -4,24 +4,35 @@ import CommonNavBar from "../Common Components/CommonNavBar";
 import { HealthContext } from "../context/HealthProvider";
 import { StFeedAddIcon } from "../styled/StyledComponents";
 import { Link } from "react-router-dom";
+import { BottomScrollListener } from "react-bottom-scroll-listener";
 
 const FeedMain = () => {
   const [allFeed, setAllFeed] = useState([]);
   const { searchedFeed } = useContext(HealthContext);
+  const [page, setPage] = useState(1);
+
+  const getFeeds = async (page) => {
+    try {
+      const { data, error } = await supabase
+        .from("feeds")
+        .select("*")
+        .range((page - 1) * 8, page * 8 - 1); //슈베에서 8개씩 범위를 지정하여 data에 할당
+      if (error) throw error;
+      setAllFeed([...allFeed, ...data]); //함수형으로 작성시 글이 중복 랜더링된 문제 수정
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    const getFeeds = async () => {
-      try {
-        const { data, error } = await supabase.from("feeds").select("*");
-        if (error) throw error;
-        console.log(data);
-        setAllFeed(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getFeeds();
-  }, []);
+    //page가 증가할때마 getFeeds호출
+    getFeeds(page);
+  }, [page]);
+
+  const handelBottomScroll = () => {
+    //BottomScrollListener를 통해 페이지의 하단일 경우 호출되는함수(페이지를 한개 추가)
+    setPage((prevPage) => prevPage + 1);
+  };
 
   return (
     <>
@@ -31,7 +42,9 @@ const FeedMain = () => {
           <div key={e.feed_id} style={{ border: "1px solid green" }}>
             <span>제목:{e.title}</span>
             <br />
-            <span>이미지:{e.img}</span>
+            <span>작성자:{e.nickname}</span>
+            <br />
+            <img src={e.content_img} style={{ width: "500px" }} />
             <br />
             <span>내용:{e.content}</span>
             <br />
@@ -41,6 +54,7 @@ const FeedMain = () => {
           </div>
         );
       })}
+      <BottomScrollListener onBottom={handelBottomScroll} />
       <Link to={"/feedadd"}>
         <StFeedAddIcon>+글작성</StFeedAddIcon>
       </Link>
