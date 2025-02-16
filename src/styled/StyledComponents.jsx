afa089b135d7851813bef2c98af11eb8b2840730
mzsoba <owt4832@gmail.com>
2025-02-16 17:55:11 +0900

    chore: 주석 추가

--- src/styled/StyledComponents.jsx
+++ src/styled/StyledComponents.jsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
-
+//페이지 메인 로고 크기
 export const StLogoIcon = styled.img`
   width: 200px;
   height: 50px;
 `;
-
+//글쓰기 버튼 디자인
 export const StFeedAddIcon = styled.div`
   border: 1px solid rgb(216, 216, 216);
   width: 80px;
@@ -19,12 +19,12 @@ export const StFeedAddIcon = styled.div`
   position: fixed;
   background-color: rgb(216, 216, 216);
 `;
-
+//마이페이지 아이콘 크기 설정
 export const StMyPageIcon = styled.img`
   width: 50px;
   height: 50px;
 `;
-
+//검색 창 위치선정
 export const StSearchBarWrapper = styled.span`
   display: flex;
   justify-content: center;
@@ -37,7 +37,7 @@ export const StSearchBarWrapper = styled.span`
     height: 20px;
   }
 `;
-
+//네비게이션 바
 export const StNavBarWrapper = styled.nav`
   display: flex;
   justify-content: space-between;
@@ -51,3 +51,4 @@ export const StNavBarWrapper = styled.nav`
   position: relative;
   height: 50px;
 `;
+
