3086dbc5a8708816293897d1bafe34431dd8f9d0
mzsoba <owt4832@gmail.com>
2025-02-16 17:04:54 +0900

    feat: navbar, 글쓰기 버튼 디자인 추가

--- src/styled/StyledComponents.jsx
+++ src/styled/StyledComponents.jsx
@@ -6,7 +6,7 @@ export const StLogoIcon = styled.img`
 `;
 
 export const StFeedAddIcon = styled.div`
-  border: 1px solid green;
+  border: 1px solid rgb(216, 216, 216);
   width: 80px;
   height: 80px;
   border-radius: 50%;
@@ -17,9 +17,37 @@ export const StFeedAddIcon = styled.div`
   bottom: 50px;
   right: 40px;
   position: fixed;
+  background-color: rgb(216, 216, 216);
 `;
 
 export const StMyPageIcon = styled.img`
   width: 50px;
+  height: 50px;
+`;
+
+export const StSearchBarWrapper = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 1;
+  input {
+    padding: 5px;
+    margin-right: 5px;
+    width: 300px;
+    height: 20px;
+  }
+`;
+
+export const StNavBarWrapper = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: 30px;
+  margin-right: 30px;
+  margin-bottom: 30px;
+  width: calc(100% - 60px);
+  background-color: rgb(216, 216, 216);
+  padding: 10px;
+  position: relative;
   height: 50px;
 `;
