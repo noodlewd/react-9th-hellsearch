62c4f658865ccd3814679dfd80e9d3a88f5fe894
mzsoba <owt4832@gmail.com>
2025-02-17 09:59:53 +0900

    feat: 드롭다운 리스트 추가, 홈버튼 클릭시 초기화 로직 추가

--- src/Common Components/CommonNavBar.jsx
+++ src/Common Components/CommonNavBar.jsx
@@ -1,7 +1,12 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { HealthContext } from "../context/HealthProvider";
 import { Link, useNavigate } from "react-router-dom";
+import React from "react";
 import {
+  StDropdownButton,
+  StDropdownItem,
+  StDropdownMenu,
+  StDropdownWrapper,
   StLogoIcon,
   StMyPageIcon,
   StNavBarWrapper,
@@ -9,10 +14,17 @@ import {
 } from "../styled/StyledComponents";
 import logo from "../assets/logoIcon.png";
 import log from "../assets/mypageIcon.png";
+import supabase from "../supabase/client";
 
-const CommonNavBar = ({ allFeed, setAllFeed }) => {
+const CommonNavBar = ({ allFeed }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
   const { searchKey, setSearchKey, setSearchedFeed } =
     useContext(HealthContext);
+
+  const isToggle = () => {
+    setIsOpen((prev) => !prev);
+  };
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
@@ -24,10 +36,43 @@ const CommonNavBar = ({ allFeed, setAllFeed }) => {
     setSearchedFeed(searchedData);
   };
 
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      // 드롭다운 리스트가 아닌 다른 곳을 클릭했을때 === 컴포넌트 언마운트시
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  const handleLogout = async () => {
+    try {
+      const { error } = await supabase.auth.signOut(); //notice: merge후 로그아웃 처리 되는지 체크해봐야함
+      if (error) throw error;
+
+      navigate("/");
+    } catch (error) {
+      console.log("로그아웃 오류", error);
+    }
+  };
+
+  const onClickHandler = () => {
+    //로고를 눌렀을때 검색창을 초기화하고 검색결과를 초기화하하는 함수
+    setSearchKey("");
+    setSearchedFeed([]);
+  };
+
   return (
     <StNavBarWrapper>
       <span>
-        <Link to={"/feedmain"}>
+        <Link to={"/feedmain"} onClick={onClickHandler}>
           <StLogoIcon src={logo} />
         </Link>
       </span>
@@ -47,8 +92,14 @@ const CommonNavBar = ({ allFeed, setAllFeed }) => {
       <Link to={"/mypage"}>
         <StMyPageIcon src={log} />
       </Link>
+      <StDropdownWrapper ref={dropdownRef}>
+        <StDropdownButton onClick={isToggle} />
+        <StDropdownMenu show={isOpen ? "true" : "false"}>
+          <StDropdownItem onClick={handleLogout}>로그아웃</StDropdownItem>
+        </StDropdownMenu>
+      </StDropdownWrapper>
     </StNavBarWrapper>
   );
 };
 
-export default CommonNavBar;
+export default React.memo(CommonNavBar);
