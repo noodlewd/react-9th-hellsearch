cf2c5a97e2b58c49c93ac9c422609632833263fb
noodlewd <michel35@naver.com>
2025-02-17 14:39:37 +0900

    test: effect 추가

--- src/pages/FeedMain.jsx
+++ src/pages/FeedMain.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { HealthContext } from "../context/HealthProvider";
 import supabase from "../supabase/client";
 import { useNavigate } from "react-router-dom";
@@ -18,6 +18,24 @@ const FeedMain = () => {
     }
   };
 
+  useEffect(() => {
+    // 사용자가 세션정보가 없는 경우 리다이렉션
+    const session = supabase.auth.getSession();
+    if (!session) {
+      alert("비정상적인 접근입니다. 로그인을 먼저 해주세요.");
+      navigate("/");
+    }
+    // 인증 상태 변경시 확인하는 리스너
+    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
+      if (!session) {
+        alert("세션이 만료 되었습니다. 다시 로그인 해주세요.");
+        navigate("/");
+      }
+    });
+    return () => {
+      authListener();
+    };
+  }, [navigate]);
   return (
     <div>
       <h2>{nickname}페이지</h2>
