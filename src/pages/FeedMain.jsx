ab77c8234f6821394f021d19c65cff6ea4fa0a95
mzsoba <owt4832@gmail.com>
2025-02-17 10:00:45 +0900

    feat: 세션 검증 로직 추가

--- src/pages/FeedMain.jsx
+++ src/pages/FeedMain.jsx
@@ -3,13 +3,14 @@ import supabase from "../supabase/client";
 import CommonNavBar from "../Common Components/CommonNavBar";
 import { HealthContext } from "../context/HealthProvider";
 import { StFeedAddIcon } from "../styled/StyledComponents";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { BottomScrollListener } from "react-bottom-scroll-listener";
 
 const FeedMain = () => {
   const [allFeed, setAllFeed] = useState([]);
   const { searchedFeed } = useContext(HealthContext);
   const [page, setPage] = useState(1);
+  const navigate = useNavigate();
 
   const getFeeds = async (page) => {
     try {
@@ -34,6 +35,30 @@ const FeedMain = () => {
     setPage((prevPage) => prevPage + 1);
   };
 
+  useEffect(() => {
+    // 사용자가 세션정보가 없는 경우 리다이렉션 && notice: 테스트시에는 이 부붑을 주석 처리할것
+    const session = supabase.auth.getSession(); //!잘은 이해 못했지만 session을 사용하면 세션객체가 null인경우 undefined가 리턴되어 예상치 못한 경우가 생겨서 getSession사용
+
+    if (!session) {
+      alert("비정상적인 접근입니다. 로그인을 먼저 해주세요.");
+      navigate("/");
+    }
+
+    // 인증 상태 변경시 확인하는 리스너
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        if (!session) {
+          alert("세션이 만료 되었습니다. 다시 로그인 해주세요.");
+          navigate("/");
+        }
+      }
+    );
+
+    return () => {
+      authListener.unsubscribe();
+    };
+  }, [navigate]);
+
   return (
     <>
       <CommonNavBar allFeed={allFeed} setAllFeed={setAllFeed} />
@@ -55,9 +80,12 @@ const FeedMain = () => {
           </div>
         );
       })}
+      {/* <FeedContent feed_id={feed_id} title={e.title} nickname={e.nickname} 
+      content_img={e.content_img} content={e.content} updated_at={e.updated_at}/>
+      이 부분을map함수안에 할당*/}
       <BottomScrollListener onBottom={handelBottomScroll} />
       <Link to={"/feedadd"}>
-        <StFeedAddIcon>+글작성</StFeedAddIcon>
+        <StFeedAddIcon />
       </Link>
     </>
   );
