9fc6c8c61c27f829ede3b6632840475d9cbb07b2
michel35_Dw <115952622+noodlewd@users.noreply.github.com>
2025-02-18 12:57:06 +0900

    Create UserMyPageCompoment .jsx

---
+++ src/styled/UserMyPageCompoment .jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+export const MyBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 80px;
+  line-height: 100px;
+`;
+export const MyTitle = styled.h2``;
+export const MyForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
+`;
+export const MyLabel = styled.label`
+  display: flex;
+  flex-direction: column;
+  line-height: 30px;
+`;
+export const MyInput = styled.input`
+  width: 400px;
+  height: 50px;
+`;
