fe3467a0bea3c669d81222b0c730af80abb03810
michel35_Dw <115952622+noodlewd@users.noreply.github.com>
2025-02-18 13:02:41 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // 기본 경로 설정
+  server: {
+    historyApiFallback: true, // 새로고침 시 404 방지
+  },
+});
